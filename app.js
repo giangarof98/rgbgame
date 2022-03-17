b7735d4cf4832a1f6076b13316ac3887e141c4d6
Gianmarco G <giangarof98@gmail.com>
2022-03-16 21:35:21 -0400

    function to generate random colors from 0 to 255

--- app.js
+++ app.js
@@ -1,15 +1,9 @@
 let squares = document.querySelectorAll('.square')
 let colorDisplay = document.getElementById('colorDisplay')
-let colors = [
-    'rgb(255, 0, 0)',
-    'rgb(255, 255, 0)',
-    'rgb(255, 0, 255)',
-    'rgb(0, 255, 0)',
-    'rgb(0, 0, 255)',
-    'rgb(0, 255, 255)',
-];
+let colors = generateRandomColors(6)
 let pickedColor = pickColor();
 let msg = document.getElementById('msg');
+let h1 = document.querySelector('h1')
 colorDisplay.textContent = pickedColor
 
 for(let i = 0; i < squares.length; i++){
@@ -19,6 +13,7 @@ for(let i = 0; i < squares.length; i++){
         if(clickedColor === pickedColor){
             msg.textContent = 'Correct'
             changeColor(clickedColor)
+            h1.style.backgroundColor = clickedColor;
         } else{
             msg.textContent = 'try again'
             this.style.backgroundColor = '#232323'
@@ -35,4 +30,21 @@ function changeColor(color){
 function pickColor(){
     let random = Math.floor(Math.random() * colors.length)
     return colors[random];
+}
+
+function generateRandomColors(num){
+    let arr = [];
+
+    for(let i = 0; i < num; i++){
+        arr.push(randomColor())
+    }
+
+    return arr;
+}
+
+function randomColor(){
+    let r = Math.floor(Math.random() * 256)
+    let g = Math.floor(Math.random() * 256)
+    let b = Math.floor(Math.random() * 256)
+    return "rgb("+ r + ', ' + g + ', ' + b +")"
 }
